6692b5dddb9bc2ad7456a51ccaff64cdef96d9c0
simpleneeraj <teamsimpleneeraj@gmail.com>
2023-02-19 18:45:28 +0530

    changes

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,15 @@ import { NODE_ENV, PORT } from '@config';
 import validateEnv from '@utils/validateEnv';
 
 validateEnv();
-
+const info = {
+  v1: '/v1',
+  get auth() {
+    return `${this.v1}/auth`;
+  },
+  get user() {
+    return `${this.v1}/user`;
+  },
+};
 const port = PORT || 5000;
 const env = NODE_ENV || 'development';
 // ROUTES
@@ -17,16 +25,9 @@ import sectionRoutes from '@/routes/series/section.route';
 import userRoutes from '@/routes/users/user.route';
 const indexRoute = HomeRoute();
 app.use(indexRoute.path, indexRoute.router);
+app.use(info.v1, indexRoute.router);
 
-const info = {
-  v1: '/v1',
-  get auth() {
-    return `${this.v1}/auth`;
-  },
-  get user() {
-    return `${this.v1}/user`;
-  },
-};
+
 
 // USER & AUTH ROUTES
 app.use(info.user, userRoutes);
